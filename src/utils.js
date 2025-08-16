ef50e7e55cebd04218875b9cd7ed6830b3fe5abe
ik04 <ishaankhurana123@gmail.com>
2025-08-17 03:50:07 +0530

    fix: fix export for commonjs

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
-export function generateMailImageUrl(domain) {
+function generateMailImageUrl(domain) {
   if (domain === "gmail.com") {
     return "/assets/gmail.webp";
   }
   return `https://www.google.com/s2/favicons?domain=${domain}&sz=128`;
 }
+
+module.exports = {
+  generateMailImageUrl,
+};
